2df6b1761e76d9f6f03a75111c217157d9cacdb8
Botaa <84439095+Botaa@users.noreply.github.com>
2022-09-15 09:06:54 +0200

    Yes

---
+++ GcCollector/GcCollector/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcCollector
+{
+    public class MyClass
+    {
+        public static int Count { get; set; }
+    }
+}
---
+++ GcCollector/GcCollector/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GcCollector
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            public static int Count { get; set; }
+            public MyClass()
+            {
+                Count++;
+            }
+             ~MyClass()
+            {
+                Count--;
+            }
+
+            public int Ki()
+            {
+                return Count;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int max = 0;
+            while(true)
+            {
+                MyClass myObj = new MyClass();
+                List<int> Lista = new List<int>();
+                Lista.Add(myObj.Ki());
+                if(max < Lista.Max())
+                    max=Lista.Max();
+
+                Console.WriteLine(max);
+            }
+        }
+    }
+}
